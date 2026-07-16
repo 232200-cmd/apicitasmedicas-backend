3f5e1fdfe8eff2216e141f98a30be9cba0662ebe
Erick Zuñiga <232200@unamba.edu.pe>
2026-07-16 11:33:36 -0500

    Arreglando CORS para Vercel

--- src/main/java/com/epiis/apicitasmedicas/WebConfig.java
+++ src/main/java/com/epiis/apicitasmedicas/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost", "http://localhost:4200")
+            .allowedOriginPatterns("*")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
